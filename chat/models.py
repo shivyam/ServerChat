f5f9003532662700c7cbe2de94b516a7a07f2b96
shivyam <144865023+shivyam@users.noreply.github.com>
2024-01-01 01:53:23 -0800

    final draft

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+#id already generated when new object is generated
+class Room(models.Model):
+    name= models.CharField(max_length=1000)
+
+
+class Message(models.Model):
+    value= models.CharField(max_length=1000000)
+    date= models.DateTimeField(default=datetime.now, blank=True)
+    user= models.CharField(max_length=10000)
+    room= models.CharField(max_length=10000)
+    
